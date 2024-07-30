97a6c61a508f47e01b9a17cb34abfc8ed999c16e
John Mason <87030324+simkidd@users.noreply.github.com>
2024-07-30 05:09:56 +0100

    test vercel

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -1,3 +1,39 @@
-import app from "../index";
+import express, { Response, Request } from "express";
+import dotenv from "dotenv";
+import { prisma } from "../libs/client";
+import subjectRoutes from "../routes/subject.routes";
+import noteTopicRoutes from "../routes/noteTopic.routes";
+import noteItemRoutes from "../routes/noteItem.routes";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+
+app.use("/api/subjects", subjectRoutes);
+app.use("/api/note-topics", noteTopicRoutes);
+app.use("/api/note-items", noteItemRoutes);
+
+// home route
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "Backend running..." });
+});
+
+const startServer = async () => {
+  try {
+    await prisma?.$connect();
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  } catch (error) {
+    console.error(error);
+    await prisma?.$disconnect();
+    process.exit(1);
+  }
+};
+
+startServer();
 
 export default app;
